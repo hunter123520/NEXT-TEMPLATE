79c46b72cacdc9746648e621409cd61728e5293f
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2025-04-02 20:42:31 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-    async redirects() {
-      return [
-        {
-          source: '/home',
-          destination: '/',
-          permanent: true, // Indicates a permanent redirect
-        },
-      ];
-    },
+
     rewrites: async () => {
       return [
         {
