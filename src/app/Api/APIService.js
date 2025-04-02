223fa7cdacf4914cdfef535db334c56773307f96
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2025-04-02 13:12:36 +0200

    flask

--- src/app/Api/APIService.js
+++ src/app/Api/APIService.js
@@ -1,7 +1,7 @@
 export default class APIService {
     // Insert an article
     static recycle(body) {
-        return fetch(`https://recycle-garbage-backend.onrender.com/recycle`, {
+        return fetch(`http://127.0.0.1:5328/api/recycle`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -12,7 +12,7 @@ export default class APIService {
         .catch(error => console.log(error))
     }
     static chat(body) {
-        return fetch(`https://recycle-garbage-backend.onrender.com/chat`, {
+        return fetch(`http://127.0.0.1:5328/api/chat`, {
             'method': 'POST',
             headers: {
                 'Content-Type': 'application/json'
