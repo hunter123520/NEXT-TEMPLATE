5bac0d3978a8318136f03a4c86c626441f4f1d24
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2025-04-08 19:02:38 +0200

    Update APIService.js

--- app/Apis/APIService.js
+++ app/Apis/APIService.js
@@ -2,7 +2,7 @@ export default class APIService {
     // http://127.0.0.1:5328
     // Insert an article
     static recycle(body) {
-        return fetch(`https://next-template-pi-kohl.vercel.app//api/recycle`, {
+        return fetch(`https://next-template-pi-kohl.vercel.app/:5328/api/recycle`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -13,7 +13,7 @@ export default class APIService {
         .catch(error => console.log(error))
     }
     static chat(body) {
-        return fetch(`https://next-template-pi-kohl.vercel.app//api/chat`, {
+        return fetch(`https://next-template-pi-kohl.vercel.app/:5328/api/chat`, {
             'method': 'POST',
             headers: {
                 'Content-Type': 'application/json'
